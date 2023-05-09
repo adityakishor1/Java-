8a25cc4c52281c2a93c42821fce1657ec9d37595
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-05-09 14:17:01 +0530

    Add files via upload

---
+++ JavaExceptionExample.java
@@ -0,0 +1,10 @@
+public class JavaExceptionExample{  
+  public static void main(String args[]){  
+   try{  
+      //code that may raise exception  
+      int data=100/0;  
+   }catch(ArithmeticException e){System.out.println(e);}  
+   //rest code of the program   
+   System.out.println("rest of the code...");  
+  }  
+}  
